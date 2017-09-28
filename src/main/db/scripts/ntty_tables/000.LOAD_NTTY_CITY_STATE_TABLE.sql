a09467a955a4a54e43d5dbc1c618aac560078e43
Spencer Bennett <spencerbennett1@SpencersMBP268.localdomain>
2017-09-28 13:59:17 -0500

    Add create by.

--- src/main/db/scripts/ntty_tables/000.LOAD_NTTY_CITY_STATE_TABLE.sql
+++ src/main/db/scripts/ntty_tables/000.LOAD_NTTY_CITY_STATE_TABLE.sql
@@ -1,9 +1,5 @@
-INSERT INTO ebdb.OTTR_ATHLETE 
-(ID, BIRTH_DATE, NAME_LAST, NAME_MIDDLE, NAME_FIRST, GENDER, CREATE_BY, MODIFY_BY) 
-VALUES (
-UNHEX(REPLACE(uuid(),'-','')), 
-(UNIX_TIMESTAMP('2005-03-30')*1000),
-'Bennett', 'Elizabeth', 'Adeline', 'F',
-(SELECT ID FROM ebdb.USR_USER a WHERE a.EMAIL = 'spencer.bennett@gmail.com'),
-(SELECT ID FROM ebdb.USR_USER a WHERE a.EMAIL = 'spencer.bennett@gmail.com')
-);
+INSERT INTO ebdb.NTTY_CITY_STATE (ID, CITY, STATE) 
+VALUES (UNHEX(REPLACE(uuid(),'-','')), 'Saint Paul', 'MN');
+
+INSERT INTO ebdb.NTTY_CITY_STATE (ID, CITY, STATE) 
+VALUES (UNHEX(REPLACE(uuid(),'-','')), 'Maplewood', 'MN');
--- src/main/db/scripts/ntty_tables/002.LOAD_NTTY_NAME_TABLE.sql
+++ src/main/db/scripts/ntty_tables/002.LOAD_NTTY_NAME_TABLE.sql
@@ -1,9 +1,15 @@
-INSERT INTO ebdb.OTTR_ATHLETE 
-(ID, BIRTH_DATE, NAME_LAST, NAME_MIDDLE, NAME_FIRST, GENDER, CREATE_BY, MODIFY_BY) 
-VALUES (
-UNHEX(REPLACE(uuid(),'-','')), 
-(UNIX_TIMESTAMP('2005-03-30')*1000),
-'Bennett', 'Elizabeth', 'Adeline', 'F',
+INSERT INTO ebdb.NTTY_NAME 
+(ID, FIRST, SECOND, THIRD, CREATE_BY, MODIFY_BY) 
+VALUES (UNHEX(REPLACE(uuid(),'-','')), 
+'Spencer', 'Hill', 'Bennett',
+(SELECT ID FROM ebdb.USR_USER a WHERE a.EMAIL = 'spencer.bennett@gmail.com'),
+(SELECT ID FROM ebdb.USR_USER a WHERE a.EMAIL = 'spencer.bennett@gmail.com')
+);
+
+INSERT INTO ebdb.NTTY_NAME 
+(ID, FIRST, SECOND, THIRD, CREATE_BY, MODIFY_BY) 
+VALUES (UNHEX(REPLACE(uuid(),'-','')), 
+'Adeline', 'Elizabeth', 'Bennett',
 (SELECT ID FROM ebdb.USR_USER a WHERE a.EMAIL = 'spencer.bennett@gmail.com'),
 (SELECT ID FROM ebdb.USR_USER a WHERE a.EMAIL = 'spencer.bennett@gmail.com')
 );
