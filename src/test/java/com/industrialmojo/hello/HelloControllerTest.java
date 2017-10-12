b675d0c1cf88db8705cc301a2c51a54d9f178184
Spencer Bennett <spencerbennett1@SpencersMBP268.localdomain>
2017-10-12 13:41:36 -0500

    More Groovy.

--- src/test/java/com/industrialmojo/hello/HelloControllerTest.java
+++ src/test/java/com/industrialmojo/hello/HelloControllerTest.java
@@ -25,13 +25,12 @@ import com.industrialmojo.Application;
 @WebAppConfiguration
 public class HelloControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+	@Autowired
+	private MockMvc mvc;
 
-    @Test
-    public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
-    }
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+	}
 }
